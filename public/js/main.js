81b6963eec4501e4725bad931ebe584ee6e3eacf
Francis Miyoba <francismiyoba@gmail.com>
2017-05-07 23:50:09 +0800

    Implement deletion logic using jquery

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,24 @@
-alert('js')
+
+//handle deletion of users
+$(document).ready(function() {
+	$('.delete-user').on('click',deleteUser)
+});
+
+function deleteUser()
+{
+	var confirmation=corfirm('Are you sure?');
+
+	if(confirmation)
+	{
+		//make an ajax call
+		$.ajax({
+			type:'DELETE',
+			url:'/users/delete/'+$('.delete-user').data('id')
+		}).done((response)=>{
+			window.location.replace('/');
+		})
+	}
+	else{
+		return false;
+	}
+}
